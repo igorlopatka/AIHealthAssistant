ab7b337d408c635f76d6812e950287f573c43556
Igor Łopatka <63002337+igorlopatka@users.noreply.github.com>
2024-05-22 12:11:00 +0200

    Initial Commit

---
+++ AIHealthAssistant/AIHealthAssistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIHealthAssistantApp.swift
+//  AIHealthAssistant
+//
+//  Created by Igor Łopatka on 22/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct AIHealthAssistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
